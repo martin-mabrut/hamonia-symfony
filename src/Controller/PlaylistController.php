c7ad75ba005a46d5ac1301bae0ca7c2799bd112d
Martin Mabrut <martinmabrut43@gmail.com>
2026-09-16 16:54:10 +0200

    Page d'une playlist

--- src/Controller/PlaylistController.php
+++ src/Controller/PlaylistController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+use App\Repository\PlaylistRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class PlaylistController extends AbstractController
 {
-    #[Route('/playlist', name: 'app_playlist')]
-    public function index(): Response
+    #[Route('/playlist/{id}', name: 'app_playlist_item')]
+    public function index($id, PlaylistRepository $playlistRepository): Response
     {
+        $playlist = $playlistRepository->find($id);
+        if($id === null){
+            return $this->redirectToRoute('app_home');
+        }
+        dump($playlist);
         return $this->render('playlist/index.html.twig', [
-            'controller_name' => 'PlaylistController',
+            'playlist' => $playlist,
         ]);
     }
 }
